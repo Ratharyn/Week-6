b4abc0bbeb634f1faad81e59f805f325dba2c35c
Ratharyn <burgesschris@hotmail.co.uk>
2026-03-22 17:58:09 +0000

    Add files via upload

---
+++ user_interface.cpp
@@ -0,0 +1,190 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+#include "user_interface.h"
+
+#include "code.h"
+#include "siren.h"
+#include "smart_home_system.h"
+#include "fire_alarm.h"
+#include "date_and_time.h"
+#include "temperature_sensor.h"
+#include "gas_sensor.h"
+#include "matrix_keypad.h"
+#include "display.h"
+
+//=====[Declaration of private defines]========================================
+
+#define DISPLAY_REFRESH_TIME_MS 1000
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut incorrectCodeLed(LED3);
+DigitalOut systemBlockedLed(LED2);
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+char codeSequenceFromUserInterface[CODE_NUMBER_OF_KEYS];
+
+//=====[Declaration and initialization of private global variables]============
+
+static bool incorrectCodeState = OFF;
+static bool systemBlockedState = OFF;
+
+static bool codeComplete = false;
+static int numberOfCodeChars = 0;
+
+
+//=====[Declarations (prototypes) of private functions]========================
+
+static void userInterfaceMatrixKeypadUpdate(char keyReleased);
+static void incorrectCodeIndicatorUpdate();
+static void systemBlockedIndicatorUpdate();
+
+static void userInterfaceDisplayInit();
+static void userInterfaceDisplayUpdate(char pressedkey);
+
+//=====[Implementations of public functions]===================================
+
+void userInterfaceInit()
+{
+    incorrectCodeLed = OFF;
+    systemBlockedLed = OFF;
+    matrixKeypadInit( SYSTEM_TIME_INCREMENT_MS );
+    userInterfaceDisplayInit();
+}
+
+void userInterfaceUpdate()
+{
+    char keypadselection = matrixKeypadUpdate();
+    userInterfaceMatrixKeypadUpdate(keypadselection); //hands the pressed key to this function
+    incorrectCodeIndicatorUpdate();
+    systemBlockedIndicatorUpdate();
+    userInterfaceDisplayUpdate(keypadselection); //hands the pressed key to this function
+}
+
+bool incorrectCodeStateRead()
+{
+    return incorrectCodeState;
+}
+
+void incorrectCodeStateWrite( bool state )
+{
+    incorrectCodeState = state;
+}
+
+bool systemBlockedStateRead()
+{
+    return systemBlockedState;
+}
+
+void systemBlockedStateWrite( bool state )
+{
+    systemBlockedState = state;
+}
+
+bool userInterfaceCodeCompleteRead()
+{
+    return codeComplete;
+}
+
+void userInterfaceCodeCompleteWrite( bool state )
+{
+    codeComplete = state;
+}
+
+//=====[Implementations of private functions]==================================
+
+static void userInterfaceMatrixKeypadUpdate(char keyReleased) //variable keypadselection is mapped to keyReleased
+{
+    static int numberOfHashKeyReleased = 0;
+    
+    if( keyReleased != '\0' ) {
+
+        if( sirenStateRead() && !systemBlockedStateRead() ) {
+            if( !incorrectCodeStateRead() ) {
+                codeSequenceFromUserInterface[numberOfCodeChars] = keyReleased;
+                numberOfCodeChars++;
+                if ( numberOfCodeChars >= CODE_NUMBER_OF_KEYS ) {
+                    codeComplete = true;
+                    numberOfCodeChars = 0;
+                }
+            } else {
+                if( keyReleased == '#' ) {
+                    numberOfHashKeyReleased++;
+                    if( numberOfHashKeyReleased >= 2 ) {
+                        numberOfHashKeyReleased = 0;
+                        numberOfCodeChars = 0;
+                        codeComplete = false;
+                        incorrectCodeState = OFF;
+                    }
+                }
+            }
+        }
+    }
+}
+
+static void userInterfaceDisplayInit()
+{
+    displayInit( DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER );
+     
+    displayCharPositionWrite ( 0,0 );
+    displayStringWrite( "Temperature:" );
+
+    displayCharPositionWrite ( 0,1 );
+    displayStringWrite( "Gas:" );
+    
+    displayCharPositionWrite ( 0,2 );
+    displayStringWrite( "Alarm:" );
+}
+
+static void userInterfaceDisplayUpdate(char pressedkey)
+{
+    char temperatureString[3] = "";
+switch (pressedkey){
+    
+    case '4':  
+        printf ("Key 4 Pressed on Keypad");//debugging line for evidence capture
+        userInterfaceDisplayInit(); //resets the screen
+
+        displayCharPositionWrite ( 4,1 ); 
+
+        if ( gasDetectorStateRead() ) {
+            displayStringWrite( "Detected    " );
+        } else {
+            displayStringWrite( "Not Detected" );
+        }
+        break; 
+
+    case '5': 
+        printf("Key 5 Pressed on Keypad"); //debugging line for evidence capture
+        userInterfaceDisplayInit(); //resets the screen
+
+        sprintf(temperatureString, "%d", (int)temperatureSensorReadCelsius());
+        displayCharPositionWrite ( 12,0 );
+        displayStringWrite( temperatureString );
+        displayCharPositionWrite ( 14,0 );
+        displayStringWrite( "'C" );
+        break; 
+
+    default:
+    break;
+ }
+}
+
+static void incorrectCodeIndicatorUpdate()
+{
+    incorrectCodeLed = incorrectCodeStateRead();
+}
+
+static void systemBlockedIndicatorUpdate()
+{
+    systemBlockedLed = systemBlockedState;
+}
+
